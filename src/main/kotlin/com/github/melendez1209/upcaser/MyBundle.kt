d07430087cd8044986b23d4ce589cdbe09816186
GitHub Action <action@github.com>
2024-01-29 04:24:00 +0000

    Template cleanup

--- src/main/kotlin/com/github/melendez1209/upcaser/MyBundle.kt
+++ src/main/kotlin/com/github/melendez1209/upcaser/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.melendez1209.upcaser
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/melendez1209/upcaser/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/melendez1209/upcaser/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.melendez1209.upcaser.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
