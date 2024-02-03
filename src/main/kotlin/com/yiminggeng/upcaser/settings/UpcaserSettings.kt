5db6967c5b87e5fadc257497ea63daab9af4aca3
Melendez <markmelendez2008@outlook.om>
2024-02-03 17:29:01 +0800

    Add settings portal and plugin icon

---
+++ src/main/kotlin/com/yiminggeng/upcaser/settings/UpcaserSettings.kt
@@ -0,0 +1,23 @@
+package com.yiminggeng.upcaser.settings
+
+import com.intellij.openapi.options.Configurable
+import com.intellij.openapi.util.NlsContexts
+import javax.swing.JComponent
+
+class UpcaserSettings : Configurable {
+    override fun getDisplayName(): @NlsContexts.ConfigurableName String? {
+        TODO("Not yet implemented")
+    }
+
+    override fun createComponent(): JComponent? {
+        TODO("Not yet implemented")
+    }
+
+    override fun isModified(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun apply() {
+        TODO("Not yet implemented")
+    }
+}
